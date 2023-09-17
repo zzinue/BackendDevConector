332de3c6ec2fbfc0dff1d2964978e0017467a843
zzinue <zzinue@gmail.com>
2023-09-17 09:39:31 -0600

    User registered

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const cors = require('cors');
+const connectDb = require('./config/db');
+// const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
+//connect db
+connectDb();
+// init middleware
+app.use(express.json({ extended: false }));
 // app.use(cors())
 // app.use(express.json())
 
 app.get('/', (req, res) => res.send('Api Running'));
+
+// // Define routees
+app.use('/api/users', require('./api/users'));
+app.use('/api/auth', require('./api/auth'));
+app.use('/api/profile', require('./api/profile'));
+app.use('/api/posts', require('./api/posts'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
